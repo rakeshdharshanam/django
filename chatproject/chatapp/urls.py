7e8561c725e238e206aa68da76693d2ca8cb08b9
rakeshdharshanam <48350862+rakeshdharshanam@users.noreply.github.com>
2022-04-30 00:09:50 +0530

    Update urls.py

--- chatproject/chatapp/urls.py
+++ chatproject/chatapp/urls.py
@@ -4,7 +4,10 @@ from django.urls import path
 from chatapp import views
 
 urlpatterns = [
-    path('',views.chat_page),
-    path('signup',views.signup),
     path('login',views.login),
+    path('loginvalidation',views.login_validation),
+    path('chathomepage',views.chat_homepage),
+    path('logout',views.logout),
+    path('newmessage',views.new_message),
+    path('refresh',views.refresh)
 ]
