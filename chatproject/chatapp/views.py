31bcecfbb6ba84234c71ff1a32e030b473b286de
rakeshdharshanam <rakesh.dharshanam333@gmail.com>
2022-04-30 10:54:49 +0000

    after modifiactions in ubuntu

--- chatproject/chatapp/views.py
+++ chatproject/chatapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import user
-
+import pymongo
 from pymongo import MongoClient
 # Create your views here.
 connection = MongoClient('localhost',27017)
